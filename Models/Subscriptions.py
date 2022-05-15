f08192ae97b905f92256c6a20135839fcbd5c19c
Константин <doom2good@mail.ru>
2022-05-15 14:06:14 +0300

    добавил группы в подписках

--- Models/Subscriptions.py
+++ Models/Subscriptions.py
@@ -4,9 +4,10 @@ from Models.Users import User
 
 
 class Subscription(BaseModel):
+    group = IntegerField(null=True)
     user = ForeignKeyField(User, backref='subscriptions', related_name='subscriptions')
-    date = DateTimeField(formats=['%Y-%m-%d %H:%M:%S'])
-    is_subscribed = BooleanField()
+    date = DateTimeField(formats=['%Y-%m-%d %H:%M:%S'], null=True)
+    is_subscribed = BooleanField(default=True)
 
     class Meta:
         table_name = 'subscriptions'
