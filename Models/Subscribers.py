a1be0e4a1e0c12ce0b88667b28bd95a0bd7b5366
Константин <doom2good@mail.ru>
2022-05-15 14:08:25 +0300

    Переименовал подписки в подписчиков

--- Models/Subscribers.py
+++ Models/Subscribers.py
@@ -3,11 +3,11 @@ from Models.base import BaseModel
 from Models.Users import User
 
 
-class Subscription(BaseModel):
+class Subscriber(BaseModel):
     group = IntegerField(null=True)
-    user = ForeignKeyField(User, backref='subscriptions', related_name='subscriptions')
+    user = ForeignKeyField(User, backref='subscriptions')
     date = DateTimeField(formats=['%Y-%m-%d %H:%M:%S'], null=True)
     is_subscribed = BooleanField(default=True)
 
     class Meta:
-        table_name = 'subscriptions'
+        table_name = 'subscribers'
