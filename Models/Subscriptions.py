10e2e1bd35637880020a5e05db029f86a6de87ff
Константин <doom2good@mail.ru>
2022-05-15 14:20:48 +0300

    вернул обратно Subscription

--- Models/Subscriptions.py
+++ Models/Subscriptions.py
@@ -3,14 +3,14 @@ from Models.base import BaseModel
 from Models.Users import User
 
 
-class Subscriber(BaseModel):
+class Subscription(BaseModel):
     group = IntegerField(null=True)
     user = ForeignKeyField(User, backref='subscriptions')
     date = DateTimeField(formats=['%Y-%m-%d'], null=True)
     is_subscribed = BooleanField(default=True)
 
     class Meta:
-        table_name = 'subscribers'
+        table_name = 'subscriptions'
 
     def __str__(self):
         state = 'ПОДПИСАН' if self.is_subscribed else 'ОТПИСАН'
