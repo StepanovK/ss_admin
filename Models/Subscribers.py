a99994be86c5f158e12924a5b466967d362b0a96
Константин <doom2good@mail.ru>
2022-05-15 14:13:15 +0300

    представление класса подписчика

--- Models/Subscribers.py
+++ Models/Subscribers.py
@@ -6,8 +6,12 @@ from Models.Users import User
 class Subscriber(BaseModel):
     group = IntegerField(null=True)
     user = ForeignKeyField(User, backref='subscriptions')
-    date = DateTimeField(formats=['%Y-%m-%d %H:%M:%S'], null=True)
+    date = DateTimeField(formats=['%Y-%m-%d'], null=True)
     is_subscribed = BooleanField(default=True)
 
     class Meta:
         table_name = 'subscribers'
+
+    def __str__(self):
+        state = 'ПОДПИСАН' if self.is_subscribed else 'ОТПИСАН'
+        return f'{self.group} от {self.date} {state}'
