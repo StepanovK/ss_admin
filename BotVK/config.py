3ba9d5871dd525714e7d3cd4295797b8e4d984e4
Константин <doom2good@mail.ru>
2022-05-23 19:40:29 +0300

    убрал лишнее

--- BotVK/config.py
+++ BotVK/config.py
@@ -7,11 +7,6 @@ logger.add('Logs/bot_log.log', format='{time} {level} {message}', rotation='512 
 env = Env()
 env.read_env()
 
-# group_name = env.str("group_name")
-group_id = env.int("group_id")
-group_token = env.str("group_token")
-telegram_bot_token = env.str("telegram_bot_token")
-telegram_chat_id = env.int("telegram_chat_id")
 admin_token = env.str("admin_token")
 admin_phone = env.str("admin_phone")
 admin_pass = env.str("admin_pass")
