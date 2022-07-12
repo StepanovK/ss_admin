eb9972fa0e0994d4ffb8185026ff54899cebfcde
sergei.mikheev <sergei.mikheev@stm-labs.ru>
2022-07-12 22:32:06 +0300

    update chat bot

--- BotVKListener/ChatBot/chat_bot.py
+++ BotVKListener/ChatBot/chat_bot.py
@@ -30,30 +30,30 @@ class ChatBot:
                                       attachment=attachment)
 
     def chat(self, event):
-        if event.object.from_id not in self.users:
-            self.users[event.object.from_id] = Commander()
+        if event.object.message.get("from_id") not in self.users:
+            self.users[event.object.message.get("from_id")] = Commander()
         # Пришло новое сообщение
         # print('Написали в ЛС')
-        answer = self.users[event.object.from_id].input(event.object.text)
-        if event.object.text == "Начать":
-            self.send_msg(event.object.random_id, event.object.peer_id,
+        answer = self.users[event.object.message.get("from_id")].input(event.object.message.get("text"))
+        if event.object.message.get("text") == "Начать":
+            self.send_msg(event.object.message.get("random_id"), event.object.message.get("peer_id"),
                           "Привет, {}!\nЯ бот группы Солнечная Сортировка!"
-                          .format(self._vk.users.get(user_id=event.object.from_id)[0]['first_name']),
-                          self.users[event.object.from_id].now_keyboard)
-        elif event.object.text == "Позвать администратора":
-            self.send_msg(event.object.random_id, event.object.peer_id,
+                          .format(self._vk.users.get(user_id=event.object.message.get("from_id"))[0]['first_name']),
+                          self.users[event.object.message.get("from_id")].now_keyboard)
+        elif event.object.message.get("text") == "Позвать администратора":
+            self.send_msg(event.object.message.get("random_id"), event.object.message.get("peer_id"),
                           answer,
-                          self.users[event.object.from_id].now_keyboard,
-                          self.users[event.object.from_id].now_attachment)
-            msg_url = 'https://vk.com/gim{}?peers=c7&sel={}'.format(config.group_id, event.object.peer_id)
-            if event.object.peer_id != self.user_call_admin:
-                self.send_msg(event.object.random_id, config.chat_for_alarm,
+                          self.users[event.object.message.get("from_id")].now_keyboard,
+                          self.users[event.object.message.get("from_id")].now_attachment)
+            msg_url = 'https://vk.com/gim{}?peers=c7&sel={}'.format(config.group_id, event.object.message.get("peer_id"))
+            if event.object.message.get("peer_id") != self.user_call_admin:
+                self.send_msg(event.object.message.get("random_id"), config.chat_for_alarm,
                               'Админы, Вас там зовут!!!\n{}'.format(msg_url),
                               keyboard=open(config.chat_bot_keyboard_path + "none.json", "r", encoding="UTF-8").read())
-                self.user_call_admin = event.object.peer_id
+                self.user_call_admin = event.object.message.get("peer_id")
 
         elif answer is not None:
-            self.send_msg(event.object.random_id, event.object.peer_id,
+            self.send_msg(event.object.message.get("random_id"), event.object.message.get("peer_id"),
                           answer,
-                          self.users[event.object.from_id].now_keyboard,
-                          self.users[event.object.from_id].now_attachment)
+                          self.users[event.object.message.get("from_id")].now_keyboard,
+                          self.users[event.object.message.get("from_id")].now_attachment)
