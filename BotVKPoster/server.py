9a767b5cdc11ae173a14748ad7a78f36b581c05c
Konstantin <doom2good@mail.ru>
2022-07-20 08:47:39 +0300

    отключил тг постера

--- BotVKPoster/server.py
+++ BotVKPoster/server.py
@@ -42,10 +42,10 @@ class Server:
         self.vk_api_group = ConnectionsHolder().vk_api_group
         self.vk = ConnectionsHolder().vk_connection_group
         self.tg_poster = None
-        try:
-            self.tg_poster = MyAutoPoster()
-        except Exception as ex:
-            logger.warning(f'Can`t connect to tg_poster: {ex}')
+        # try:
+        #     self.tg_poster = MyAutoPoster()
+        # except Exception as ex:
+        #     logger.warning(f'Can`t connect to tg_poster: {ex}')
 
     def _start_polling(self):
 
