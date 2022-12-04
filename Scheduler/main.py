1f3c5bbf24c67f40c7eb00e40e7f40359dba7236
sergei.mikheev <sergei.mikheev@stm-labs.ru>
2022-12-04 16:44:47 +0300

    add dynamic title manager

--- Scheduler/main.py
+++ Scheduler/main.py
@@ -4,9 +4,11 @@ from config import logger
 
 from utils.Scripts.ConversationsCleaning.cleaner import start_cleaning as conversation_cleaning
 from utils.Scripts.ADS_Manager.ads_manager import check_ads_posts
+from utils.Scripts.Dynamic_title_vk.dynamic_title_manager import update_title_vk
 
 schedule.every(30).minutes.do(conversation_cleaning)
 schedule.every(10).minutes.do(check_ads_posts)
+schedule.every(10).minutes.do(update_title_vk)
 
 logger.error(f'Starting...')
 while True:
